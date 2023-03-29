daf52e69aace3faad8d6f53d5c2c802b1f2fa4c3
Luiz Gustavo <jhhhhhhh58@gmail.com>
2023-03-29 17:26:12 -0300

    .
    passo2

--- PrimeiroPasso.py
+++ PrimeiroPasso.py
@@ -1,19 +1,55 @@
 #Primeiro Passo - Abrir o caixa
 
-senha = 1234
+print(f"\tBem-Vindo!!!")
 
-print ("\n\n\t\t Bem-vindo !! \t\t\n\n")
-t=3
-while t>0:
-    t-=1
-    senha2 = int(input("Digite sua senha para abrir o caixa: "))
-    if senha2!=senha:
-        print("Senha Incorreta!")
-        print(f"Você tem direito a mais {t} tentativas")
-        if t==0:
-            print('Sistema deve ser Reinicializado')
-    else:
-        print("Senha Correta!!")
-        print("Caixa Aberto!!")
-        break
+password_correta = 1234
+ten = 3
 
+while ten > 0:
+
+        password = int(input("Dígite sua senha para abrir o caixa: "))
+        
+        if password != password_correta:
+        
+            if ten == 1:
+                break
+            else:
+                ten -= 1
+            print(f"VC TEM DIREITO A MAIS {ten} TENTATIVAS")
+             
+        else:
+            ten = 0
+            print(f"Senha Correta \n Caixa Aberto!")
+            print(f"Dinheiro para troco: R$1280,00")
+
+            print("INSERÇÃO DOS ITENS VENDIDOS")
+
+            val = 0
+            itn = 0
+            val_itn = 0
+            total = 0
+
+            while val == 0:
+                 itn += 1
+                 val_itn = float(input(f"Digite o valor do item {itn}: R$"))
+                 total += val_itn
+
+
+                 if val_itn == 0:
+                    
+                    opc = input(f"Deseja Realmente Finalizar?? (S= Sim e N= Não)\n")
+                    
+                    if opc == "S":
+                         itn -= 1
+                         print(f"Venda Finalizada {itn} itens")
+                         print(total)
+                         val = 1
+                         
+                    else:
+                         itn -=1
+                         val = 0
+                 elif val_itn == -1:
+                         
+                         itn -= 1
+                         val_cor = float(input(f"Escreva o valor correto do iten {itn}: R$"))
+                         
