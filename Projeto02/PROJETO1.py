d769fb7733612c64d3e7602a55e8a34b3251f203
cissa-eduarda <ciceraeduardac@gmail.com>
2023-04-03 07:44:48 -0300

    Concerto do erro no segundo

--- Projeto02/PROJETO1.py
+++ Projeto02/PROJETO1.py
@@ -23,12 +23,23 @@ while t<3:
         break
 
 #Troco inicial 
-notas200 = 200
-notas100 = 100
-notas50 = 50
-notas10 = 10
-notas5 = 5
-notas05 = 0.5
+
+#numero de notas
+n_200 = 2
+n_100 = 4
+n_50 = 6
+n_10 = 10
+n_5 = 10
+n_1 = 20
+n_05 = 20
+
+notas200 = 200 * n_200
+notas100 = 100 * n_100
+notas50 = 50 * n_50
+notas10 = 10 * n_10
+notas5 = 5 * n_5
+notas1 = 1 * n_1
+notas05 = 0.5 * n_05
 
 #Segundo Passo - Leitura das Vendas
 
@@ -39,38 +50,54 @@ print("Para alterar o valor de um item digite '-1' \n\n")
 
 #quantidade dos itens vendidos
 item = 0
-soma_itens = 0
 #soma total dos itens
 soma_valor = 0
 valor = 0
-fim = "S"
+fim = "N"
 
 #Insercao dos Itens Vendidos
 
 while fim == "N" or fim =="n":
         
         item+=1
-        soma_valor+= valor
 
+        soma_valor+= valor
         valor = float(input(f"Digite o valor do Item {item}: R$"))
+
         if valor == 0:
             fim = str(input("Deseja Realmente Finalizar?? (S=Sim e N=Nao): "))
+            if fim == "S" or fim == "s":
+                print(f"Venda Finalizada com {item} ")
+
+        elif valor <-1:
+            soma_valor-=valor
+            print ("Digite um valor valido!")
         elif valor ==-1:
             soma_valor-=valor
             valor = float(input("Digite o valor correto do item anterior: R$"))
-        elif fim == "S" or fim =="s":
-            print(f"Venda Finalizada com {soma_itens}")
-            break
-
-
-        
-
 
 #TERCEIRO PASSO - Finalizacao da venda e calculo do troco
 
 
+print (f"Valor total: {soma_valor:.2f}")
+valor_pago = (float(input("Insira o valor pago: ")))
+
+if valor_pago == soma_valor:
+    print("Nao há troco!!")
+
+elif valor_pago < soma_valor:
+    print("Valor insuficiente!")
+
+elif valor_pago>soma_valor:
+    troco = valor_pago - soma_valor
+    print(f"Troco: {troco:.2f} ")
+
+#troco
+#         
 
 
+
+    
 
 
 
