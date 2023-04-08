1babbfadda0dc8e24cd9ddf7e9eb7c82226b2b71
Luiz Gustavo <jhhhhhhh58@gmail.com>
2023-04-07 23:50:45 -0300

    .

--- Projeto02/Anexos/Teste_Validacao.py
+++ Projeto02/Anexos/Teste_Validacao.py
@@ -1,10 +1,6 @@
+dado = int(input("Digite um dado: "))
 
-nota = -1
-while nota < 0 or nota > 10:
-    try:
-        nota = int(input("Informe a nota entre 0 e 10: "))
-        print("A nota foi:", nota)
-        break
-    except:
-        print("Valor inválido")
-        
+if type(dado) == int:
+    print("O dado inserido é um número inteiro.")
+else:
+    print("Sucesso cissa")
